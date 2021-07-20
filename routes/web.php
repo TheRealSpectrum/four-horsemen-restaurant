dc7209d18b5434b44dd5eb22bcb908577fe39ad4
Damy Metzke <damy.metzke@gmail.com>
2021-07-20 11:56:41 +0200

    Add `AuthController`

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AuthController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get("/", function () {
     return view("welcome");
+});
+
+Route::name("auth.")->group(function () {
+    Route::get("/login", [AuthController::class, "login"])->name("login");
+    Route::post("/login", [AuthController::class, "authenticate"])->name(
+        "authenticate"
+    );
+    Route::post("/logout", [AuthController::class, "logout"])->name("logout");
 });
