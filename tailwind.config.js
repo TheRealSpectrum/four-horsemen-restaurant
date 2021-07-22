8aee41491ce436f00a7088b90e45649871a2f4bd
Damy Metzke <damy.metzke@gmail.com>
2021-07-22 13:40:17 +0200

    Write `auth.login` view

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,4 +7,22 @@ module.exports = {
         },
     },
     plugins: [],
+    theme: {
+        colors: {
+            light: "#f7f5e8",
+            "light-gray": "#becec0",
+            "dark-gray": "#5e696b",
+            dark: "#010521",
+            highlight: "#162f9e",
+            warning: {
+                high: "#ff7272",
+                low: "#ffcf72",
+            },
+            save: "#25FD02",
+        },
+        fontFamily: {
+            sans: ["Oxygen", "sans-serif"],
+            serif: ["'Noto Serif'", "serif"],
+        },
+    },
 };
