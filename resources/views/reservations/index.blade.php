ddc472e5c33eb47f900cf0a2f6e5230362f430ef
rik jilleba <79363708+etheralda@users.noreply.github.com>
2021-08-17 15:03:26 +0200

    start basic column sort (need change)

--- resources/views/reservations/index.blade.php
+++ resources/views/reservations/index.blade.php
@@ -1,7 +1,24 @@
+@php
+date_default_timezone_set("Europe/Amsterdam");
+$now = new DateTime(date('Y-m-d H:i'));
+$plus1hours = new DateTime(date("+1hour"))
+@endphp
+
 @extends("layout.app")
 
 @section("content")
     @foreach ($resevations as $item)
-        <x-res-item :info="$item"></x-reservation-item>
+        @php
+        $reservationDate = new DateTime(date('Y-m-d H:i', strtotime("$item->date $item->time")));
+        @endphp
+        @if ($reservationDate > $now && $reservationDate < $plus1hours)
+            <x-res-item :info="$item" :pos="mid lower"></x-reservation-item>
+        @elseif ($reservationDate < $now && $item->active)
+            <x-res-item :info="$item" :pos="left"></x-reservation-item>
+        @elseif ($reservationDate < $now && !$item->active)
+            <x-res-item :info="$item" :pos="mid upper"></x-reservation-item>
+        @else
+            <x-res-item :info="$item" :pos="right"></x-reservation-item>
+        @endif
     @endforeach
 @endsection()
