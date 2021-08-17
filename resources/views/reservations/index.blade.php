a8fd76bf7dbc6f828df72dba2358cbfc1ba78481
rik jilleba <79363708+etheralda@users.noreply.github.com>
2021-08-17 15:57:42 +0200

    add basic columns

--- resources/views/reservations/index.blade.php
+++ resources/views/reservations/index.blade.php
@@ -12,13 +12,29 @@ $plus1hours = new DateTime(date("+1hour"))
         $reservationDate = new DateTime(date('Y-m-d H:i', strtotime("$item->date $item->time")));
         @endphp
         @if ($reservationDate > $now && $reservationDate < $plus1hours)
-            <x-res-item :info="$item" :pos="mid lower"></x-reservation-item>
-        @elseif ($reservationDate < $now && $item->active)
-            <x-res-item :info="$item" :pos="left"></x-reservation-item>
-        @elseif ($reservationDate < $now && !$item->active)
-            <x-res-item :info="$item" :pos="mid upper"></x-reservation-item>
+            <x-res-item :info="$item"></x-reservation-item>
+        @elseif ($reservationDate < $now && $item->status == true)
+            <x-res-item :info="$item"></x-reservation-item>
+        @elseif ($reservationDate < $now && $item->status == false)
+            <x-res-item :info="$item" ></x-reservation-item>
         @else
-            <x-res-item :info="$item" :pos="right"></x-reservation-item>
+            <x-res-item :info="$item"></x-reservation-item>
         @endif
     @endforeach
+<div id="main_wrapper" class="grid grid-cols-4 grid-rows-1 gap-5 ">
+    <div id="active_reservations" class="col-start-1 col-span-1 flex flex-row flex-wrap border-2 border-black">
+        <p>active reservations</p>
+    </div>
+    <div id="incoming_reservations" class="col-start-2 col-span-2 flex flex-col flex-nowrap border-2 border-black">
+        <div id="late_reservations" class="flex flex-row flex-wrap border-2 border-black">
+            <p>late reservations</p>
+        </div>
+        <div id="upcoming_reservations" class="flex flex-row flex-wrap border-2 border-black">
+            <p>reservations in the next hour</p>
+        </div>
+    </div>
+    <div id="later_reservations" class="col-start-4 col-span-1 flex flex-row flex-wrap border-2 border-black">
+        <p>reservations later today</p>    
+    </div>
+</div>
 @endsection()
