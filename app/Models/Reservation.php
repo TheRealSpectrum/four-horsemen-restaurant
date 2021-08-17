2bbb741711ccdd3da0f1f5372407d078bf4582b8
Damy Metzke <damy.metzke@gmail.com>
2021-08-17 15:25:59 +0200

    Implement `ReservationFactory`

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -5,7 +5,21 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Reservation extends Model
+final class Reservation extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        "name",
+        "guest_count",
+        "date_start",
+        "date_end",
+        "active",
+        "event_type",
+    ];
+
+    protected $casts = [
+        "date_start" => "datetime",
+        "date_end" => "datetime",
+    ];
 }
--- database/factories/ReservationFactory.php
+++ database/factories/ReservationFactory.php
@@ -5,24 +5,23 @@ namespace Database\Factories;
 use App\Models\Reservation;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-class ReservationFactory extends Factory
+final class ReservationFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
     protected $model = Reservation::class;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
-    public function definition()
+    public function definition(): array
     {
+        $date = $this->faker->dateTimeBetween("+1 hour", "+4 days");
+        $dateEnd = clone $date;
+        $dateEnd->add(new \DateInterval("PT1H"));
+
         return [
-            //
+            "name" => $this->faker->name(),
+            "guest_count" => $this->faker->numberBetween(1, 6),
+            "date_start" => $date,
+            "date_end" => $dateEnd,
+            "active" => true,
+            "event_type" => "",
         ];
     }
 }
