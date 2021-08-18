90abddc8a45ae0a987a83d7510b8cdc896f30b5d
Damy Metzke <damy.metzke@gmail.com>
2021-08-18 16:04:49 +0200

    Add `table` model and related
    Write migration.

---
+++ database/migrations/2021_08_17_135011_create_tables_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+final class CreateTablesTable extends Migration
+{
+    public function up(): void
+    {
+        Schema::create("tables", function (Blueprint $table) {
+            $table->unsignedBigInteger("id");
+            $table->primary("id");
+
+            $table->integer("seat_count");
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists("tables");
+    }
+}
