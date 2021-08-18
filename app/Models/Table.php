5cf3009d590d4b45ef1b1f68e0783ff7d278722d
Damy Metzke <damy.metzke@gmail.com>
2021-08-18 17:20:43 +0200

    Implement `Table` model (untested)

--- app/Models/Table.php
+++ app/Models/Table.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
-class Table extends Model
+final class Table extends Model
 {
     use HasFactory;
+
+    public function reservations(): BelongsToMany
+    {
+        return $this->belongsToMany(Reservation::class);
+    }
+
+    protected $fillable = ["id", "seat_count"];
 }
