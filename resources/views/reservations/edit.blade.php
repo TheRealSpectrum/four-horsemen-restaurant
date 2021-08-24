c38f71e5c27e5f384f7753ae84dfc512f83ecf46
rik jilleba <79363708+etheralda@users.noreply.github.com>
2021-08-24 11:16:07 +0200

    remove edit contend for vue replacement

--- resources/views/reservations/edit.blade.php
+++ resources/views/reservations/edit.blade.php
@@ -5,19 +5,7 @@ date_default_timezone_set("Europe/Amsterdam");
 @extends("layout.app")
 
 @section("content")
-<div id="main_wrapper" class="grid grid-cols-10 grid-rows-1 gap-5 ">
-    <p class="row-start-1 col-span-2">active reservations</p>
-    <div id="active_reservations" class="row-start-2 col-start-1 col-span-2 flex flex-row flex-wrap border-2 border-black place-content-start">
-    </div>
-    <div id="incoming_reservations" class="row-start-2 col-start-3 col-span-6 flex flex-col flex-nowrap border-2 border-black place-content-start">
-        <div class="search">
-            <x-labled-input :data=""></x-reservation-item>
-        </div>
-        <div class="results"></div>
-        <div class="edit"></div>
-    </div>
-    <p class="row-start-1 col-start-9 col-span-2">reservations later today</p>
-    <div id="later_reservations" class="row-start-2 col-start-9 col-span-2 flex flex-row flex-wrap border-2 border-black place-content-start">
-    </div>
+<div id="vue_wraper" class="">
+
 </div>
 @endsection()
