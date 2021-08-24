a3ad6da71f9bbd8f07782ae7662d194cff6d9bb9
rik jilleba <79363708+etheralda@users.noreply.github.com>
2021-08-24 09:39:21 +0200

    fix for pull

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -36,6 +36,6 @@ class ReservationController extends Controller
     }
     public function edit(){
         $data = Reservation::all();
-        return view("reservations.edit",["reservations"=>$data])
+        return view("reservations.edit",["reservations"=>$data]);
     }
 }
