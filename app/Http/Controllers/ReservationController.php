eecd922c85a22718b3d0358b9a83a3e67de97091
rik jilleba <79363708+etheralda@users.noreply.github.com>
2021-08-26 12:21:19 +0200

    write controler to pass table and privot data

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Reservation;
+use App\Models\Table;
 use Illuminate\Http\Request;
 
 class ReservationController extends Controller
@@ -53,6 +54,13 @@ class ReservationController extends Controller
     public function edit()
     {
         $data = Reservation::all();
-        return view("reservations.edit", ["data" => $data]);
+        $tables = Table::all();
+        $pivot = [];
+        foreach($data as $reservation){
+            foreach($reservation->tables as $table){
+                array_push($pivot,["reservation_id"=>$reservation->id,"table_id"=>$table->id]);
+            }
+        }
+        return view("reservations.edit", ["data" => $data, "tables" => $tables, "pivot"=>$pivot]);
     }
 }
