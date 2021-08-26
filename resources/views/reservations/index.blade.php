0c016e865ee955658970d818df980ee331c4ccc3
Damy Metzke <damy.metzke@gmail.com>
2021-08-26 12:28:46 +0200

    Implement show box dynamic popup

--- resources/views/reservations/index.blade.php
+++ resources/views/reservations/index.blade.php
@@ -47,38 +47,38 @@
 
     </div>
 </div>
-<div id="show"
-    class="fixed w-screen h-screen inset-0 p-52 bg-dim">
+<div id="show-box-root" data-show-box-json="{{App\Models\Reservation::formatCollectionForJavascript($reservations)}}"
+    class="fixed w-screen h-screen inset-0 p-52 bg-dim hidden">
 
     <div class="bg-light w-full h-full border-2 border-dark p-4 dark:border-light dark:bg-dark">
         <h1 class="mx-auto text-center text-4xl font-bold">reservation information</h1>
 
         <div class="flex flex-row justify-end h-full items-stretch">
-            <div id="reservation-data" data-reservations="{{App\Models\Reservation::formatCollectionForJavascript($reservations)}}"
+            <div id="reservation-data" 
                 class="flex flex-col gap-6 pt-6">
                 <div class="ml-32 grid grid-cols-5">
                     <p class="text-2xl font-bold w-30 text-right pr-4 row-span-1">Name:</p>
-                    <p data-listing-field="" class="reservation-data-value text-2xl text-dark-gray row-span-4 dark:text-light-gray"></p>
+                    <p data-show-box-display="name" class="show-box-display text-2xl text-dark-gray row-span-4 dark:text-light-gray"></p>
                 </div>
                 <div class="ml-32 grid grid-cols-5">
                     <p class="text-2xl font-bold w-30 text-right pr-4 row-span-1">Number of guests:</p>
-                    <p data-listing-field="" class="reservation-data-value text-2xl text-dark-grey row-span-5"></p>
+                    <p data-show-box-display="guestCount" class="show-box-display text-2xl text-dark-grey row-span-5"></p>
                 </div>
                 <div class="ml-32 grid grid-cols-5">
                     <p class="text-2xl font-bold w-30 text-right pr-4 row-span-1">Day:</p>
-                    <p data-listing-field="" class="reservation-data-value text-2xl text-dark-grey row-span-5"></p>
+                    <p data-show-box-display="dateStart" class="show-box-display text-2xl text-dark-grey row-span-5"></p>
                 </div>
                 <div class="ml-32 grid grid-cols-5">
                     <p class="text-2xl font-bold w-30 text-right pr-4 row-span-1">Starting time:</p>
-                    <p data-listing-field="" class="reservation-data-value text-2xl text-dark-grey row-span-5"></p>
+                    <p data-show-box-display="timeStart" class="show-box-display text-2xl text-dark-grey row-span-5"></p>
                 </div>
                 <div class="ml-32 grid grid-cols-5">
                     <p class="text-2xl font-bold w-30 text-right pr-4 row-span-1">Has arrived:</p>
-                    <p data-listing-field="" class="reservation-data-value text-2xl text-dark-grey row-span-5"></p>
+                    <p data-show-box-display="active" class="show-box-display text-2xl text-dark-grey row-span-5"></p>
                 </div>
                 <div class="ml-32 grid grid-cols-5">
                     <p class="text-2xl font-bold w-30 text-right pr-4 row-span-1">Event:</p>
-                    <p data-listing-field="" class="reservation-data-value text-2xl text-dark-grey row-span-5"></p>
+                    <p data-show-box-display="eventType" class="show-box-display text-2xl text-dark-grey row-span-5"></p>
                 </div>
             </div>
 
