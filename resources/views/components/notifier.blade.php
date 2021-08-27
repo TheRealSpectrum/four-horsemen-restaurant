e2c57194fe5f3803f3b91dad46ba2d6145f8a60f
Damy Metzke <damy.metzke@gmail.com>
2021-08-27 14:49:58 +0200

    Add rounded corners to notifications

--- resources/views/components/notifier.blade.php
+++ resources/views/components/notifier.blade.php
@@ -1,5 +1,5 @@
 <div id="{{$notifyId}}" data-instant-trigger="{{$instantTrigger}}"
-    class="notifier bg-{{$colorName}} border-2 border-dark dark inline-block p-2 right-6 top-6 fixed w-40">
+    class="notifier bg-{{$colorName}} border-2 border-dark dark inline-block p-2 right-6 top-6 fixed w-40 rounded-xl">
     <div class="text-center text-xl">{{$titleText}}</div>
     <div class="text-center">{{$slot}}</div>
 </div>
