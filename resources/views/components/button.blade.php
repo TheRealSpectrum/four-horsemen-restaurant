f5d6fda41003022b30967dd959b626400fce9cce
Damy Metzke <damy.metzke@gmail.com>
2021-09-01 16:35:24 +0200

    Implement hover effect for `button` component

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,3 +1,5 @@
-<button type="{{$buttonType}}" class="{{$additionalClasses}} border-2 border-dark p-1 rounded">
-    {{$slot}}
+<button type="{{$buttonType}}" class="{{$additionalClasses}} border-2 border-dark rounded">
+    <div class="hover:bg-button-dim w-full h-full p-1">
+        {{$slot}}
+    </div>
 </button>
