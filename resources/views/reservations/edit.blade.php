10f57aa98a649bb9783d9a9a67abced4a81f6282
rik jilleba <79363708+etheralda@users.noreply.github.com>
2021-09-02 11:46:09 +0200

    edit styles by adding a class to `#app`

--- resources/views/reservations/edit.blade.php
+++ resources/views/reservations/edit.blade.php
@@ -6,7 +6,7 @@ date_default_timezone_set("Europe/Amsterdam");
 
 @section("content")
 <meta name="csrf-token" content="{{ csrf_token() }}">
-<div id="app" class="">
+<div id="app" class="search">
     <edit-component  :reservation_data="{{($data)}}" :table_data="{{($tables)}}" :pivot="{{json_encode($pivot)}}"></example-component>
 
 </div>
