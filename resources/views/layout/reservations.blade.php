378fd898770b479f2f042fd88c2cbc852551b6af
Damy Metzke <damy.metzke@gmail.com>
2021-09-03 11:50:17 +0200

    Add `layout.reservations` view

---
+++ resources/views/layout/reservations.blade.php
@@ -0,0 +1,20 @@
+@extends("layout.app")
+
+@section("content")
+
+    <div class="w-full h-20 fixed left-0 top-0
+        grid grid-cols-3">
+        <div></div>
+        <div class="flex flex-row justify-between content-evenly gap-2 px-6 py-6">
+            <a href="{{route("reservations.index")}}" class="w-full inline-block"><x-button class="w-full">Today</x-button></a>
+            <a href="{{route("reservation.create")}}" class="w-full inline-block"><x-button class="w-full">Create reservation</x-button></a>
+            <a href="{{route("reservation.edit")}}" class="w-full inline-block"><x-button class="w-full">All reservations</x-button></a>
+        </div>
+        <div></div>
+
+    </div>
+    <div class="w-full h-20"></div>
+
+    @yield("reservation-content")
+
+@endsection
