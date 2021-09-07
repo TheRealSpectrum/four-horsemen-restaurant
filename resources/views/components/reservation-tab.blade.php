a92da569f49f58ddaea9d726e8a2decbe84c8b45
Shadow-Hunter-git <gavi_100@msn.com>
2021-09-07 13:13:17 +0200

    styling aangepast met scrollen en heights

--- resources/views/components/reservation-tab.blade.php
+++ resources/views/components/reservation-tab.blade.php
@@ -1,4 +1,4 @@
 <div {{$attributes}} class="{{$additionalClasses}} flex flex-col">
     <div class="bg-light-gray h-8 w-64 pl-4">{{$title}}</div>
-    <div class="{{$additionalBodyClasses}} grid bg-light-gray h-auto p-4 gap-x-5 gap-y-3 auto-rows-min flex-1">{{$slot}}</div>
+    <div class="{{$additionalBodyClasses}} grid bg-light-gray h-auto p-4 gap-x-5 gap-y-3 auto-rows-min flex-1" style="height:90%; overflow:auto">{{$slot}}</div>
 </div>
