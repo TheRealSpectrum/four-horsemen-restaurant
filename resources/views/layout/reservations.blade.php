a60af325f83f31b8054db1f455fc3b657d207912
Damy Metzke <damy.metzke@gmail.com>
2021-09-07 19:53:19 +0200

    Update `layout.app` view

--- resources/views/layout/reservations.blade.php
+++ resources/views/layout/reservations.blade.php
@@ -6,14 +6,14 @@
         grid grid-cols-3">
         <div></div>
         <div class="flex flex-row justify-between content-evenly gap-2 px-6 py-6">
-            <a href="{{route("reservations.index")}}" class="w-full inline-block"><x-button class="w-full">Today</x-button></a>
-            <a href="{{route("reservation.create")}}" class="w-full inline-block"><x-button class="w-full">Create reservation</x-button></a>
-            <a href="{{route("reservation.edit")}}" class="w-full inline-block"><x-button class="w-full">All reservations</x-button></a>
+            <a href="{{route("reservations.index")}}" class="w-full inline-block"><x-button level="nav" class="w-full">Today</x-button></a>
+            <a href="{{route("reservation.create")}}" class="w-full inline-block"><x-button level="nav" class="w-full">Create reservation</x-button></a>
+            <a href="{{route("reservation.edit")}}" class="w-full inline-block"><x-button level="nav" class="w-full">All reservations</x-button></a>
         </div>
         <div>
             <form action="{{route("auth.logout")}}" method="post" class="flex flex-row justify-end h-full py-6 px-6">
                 @csrf
-                <x-button type="submit">Logout</x-button>
+                <x-button type="submit" level="nav">Logout</x-button>
             </form>
         </div>
 
