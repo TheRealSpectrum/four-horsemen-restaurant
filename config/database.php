e974a69b55e6259442b99847e1127c1bc72bf7d3
Damy Metzke <damy.metzke@gmail.com>
2021-09-10 10:44:07 +0200

    Possible fix for test workflow

--- config/database.php
+++ config/database.php
@@ -63,27 +63,42 @@ return [
                 : [],
         ],
 
-        "mysql_test" => [
-            "driver" => "mysql",
-            "url" => env("DATABASE_URL"),
-            "host" => env("DB_HOST", "127.0.0.1"),
-            "port" => env("DB_PORT", "3306"),
-            "database" => env("DB_DATABASE_TEST", "restaurant_app_test"),
-            "username" => env("DB_USERNAME", "sail"),
-            "password" => env("DB_PASSWORD", "password"),
-            "unix_socket" => env("DB_SOCKET", ""),
-            "charset" => "utf8mb4",
-            "collation" => "utf8mb4_unicode_ci",
-            "prefix" => "",
-            "prefix_indexes" => true,
-            "strict" => true,
-            "engine" => null,
-            "options" => extension_loaded("pdo_mysql")
-                ? array_filter([
-                    PDO::MYSQL_ATTR_SSL_CA => env("MYSQL_ATTR_SSL_CA"),
-                ])
-                : [],
-        ],
+        "mysql_test" =>
+            env("CI_ENABLED", "false") === "true"
+                ? [
+                    "driver" => "sqlite",
+                    "url" => env("DATABASE_URL"),
+                    "database" => env(
+                        "DB_DATABASE",
+                        database_path("database.sqlite")
+                    ),
+                    "prefix" => "",
+                    "foreign_key_constraints" => env("DB_FOREIGN_KEYS", true),
+                ]
+                : [
+                    "driver" => "mysql",
+                    "url" => env("DATABASE_URL"),
+                    "host" => env("DB_HOST", "127.0.0.1"),
+                    "port" => env("DB_PORT", "3306"),
+                    "database" => env(
+                        "DB_DATABASE_TEST",
+                        "restaurant_app_test"
+                    ),
+                    "username" => env("DB_USERNAME", "sail"),
+                    "password" => env("DB_PASSWORD", "password"),
+                    "unix_socket" => env("DB_SOCKET", ""),
+                    "charset" => "utf8mb4",
+                    "collation" => "utf8mb4_unicode_ci",
+                    "prefix" => "",
+                    "prefix_indexes" => true,
+                    "strict" => true,
+                    "engine" => null,
+                    "options" => extension_loaded("pdo_mysql")
+                        ? array_filter([
+                            PDO::MYSQL_ATTR_SSL_CA => env("MYSQL_ATTR_SSL_CA"),
+                        ])
+                        : [],
+                ],
 
         "pgsql" => [
             "driver" => "pgsql",
