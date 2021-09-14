0fa4e00cca5e7cd0ebf9e7f56ff22d1e1785e757
Damy Metzke <damy.metzke@gmail.com>
2021-09-14 15:14:07 +0200

    Implement `manager.*.destroy`

--- resources/views/management/show.blade.php
+++ resources/views/management/show.blade.php
@@ -1,6 +1,17 @@
 Management: show
 
 <div>
-  <div>{{$model->name}}</div>
-  <div>{{$model->priceAsString()}}</div>
+    @foreach($columns as $column)
+      @php($columnName = $column->name)
+
+      <div>Field: {{ $column->display }}</div>
+      <div>Value: {{ $model->$columnName }}</div>
+
+    @endforeach
 </div>
+
+<form action="{{ route("management.$managementName.destroy", [$managementParameterName => $model->id]) }}" method="post">
+  @csrf
+  @method("DELETE")
+  <x-button level="warningHigh" type="submit">Delete</x-button>
+</form>
