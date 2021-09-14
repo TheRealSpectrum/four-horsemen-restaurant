eeef72fef8efded2c22b7ffc813dddf2bbca29b3
Damy Metzke <damy.metzke@gmail.com>
2021-09-14 14:09:44 +0200

    Implement column registration for management

--- app/Http/Controllers/Management/ManagementController.php
+++ app/Http/Controllers/Management/ManagementController.php
@@ -3,32 +3,39 @@
 namespace App\Http\Controllers\Management;
 
 use App\Http\Controllers\Controller;
+use App\Management\ManagementColumn;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 
 abstract class ManagementController extends Controller
 {
     public function index(): View
     {
+        $this->managementInitWrapper();
         $models = $this->GetModelBuilder()
             ->orderBy("name")
             ->get();
 
         return view("management.index", [
             "models" => $models,
+            "columns" => $this->registeredColumns,
         ]);
     }
 
     public function create(): View
     {
-        return view("management.create");
+        return view("management.create", [
+            "managementName" => $this->managementName,
+        ]);
     }
 
     public function store(Request $request): RedirectResponse
     {
+        return redirect()->route("management.$this->managementName.index");
     }
 
     public function show($id): view
@@ -64,6 +71,34 @@ abstract class ManagementController extends Controller
     }
 
     protected string $managementModel = "";
+    protected string $managementName = "";
+
+    abstract protected function managementInit(): void;
+
+    protected function RegisterColumn(
+        string $name,
+        string $display,
+        string $type,
+        array $validationRules = [],
+        bool $showInIndex = true
+    ): self {
+        $this->registeredColumns->push(
+            new ManagementColumn(
+                $name,
+                $display,
+                $type,
+                $validationRules,
+                $showInIndex
+            )
+        );
+        return $this;
+    }
+
+    private function managementInitWrapper()
+    {
+        $this->registeredColumns = new Collection();
+        $this->managementInit();
+    }
 
     private function GetModelBuilder(): Builder
     {
@@ -71,4 +106,6 @@ abstract class ManagementController extends Controller
             ->getMethod("query")
             ->invoke(null);
     }
+
+    private Collection $registeredColumns;
 }
--- resources/views/management/create.blade.php
+++ resources/views/management/create.blade.php
@@ -1,1 +1,11 @@
 Management: create
+
+<form action="{{route("management.$managementName.store")}}" method="post">
+  @csrf
+  <label for="name">Name</label>
+  <input type="text" name="name">
+  <label for="price">Price</label>
+  <input type="number" name="price">
+
+  <x-button type="submit">Create dish</x-button>
+</form>
