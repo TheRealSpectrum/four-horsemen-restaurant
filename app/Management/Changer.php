dc0347a565f0e9d427d5b5c1a3195092c7ea0120
Damy Metzke <damy.metzke@gmail.com>
2021-09-17 10:29:22 +0200

    Reimplement `management.*.store`

--- app/Management/Changer.php
+++ app/Management/Changer.php
@@ -4,7 +4,7 @@ namespace App\Management;
 
 final class Changer
 {
-    public function __contruct(string $column, array $validation)
+    public function __construct(string $column, array $validation)
     {
         $this->column = $column;
         $this->validation = $validation;
