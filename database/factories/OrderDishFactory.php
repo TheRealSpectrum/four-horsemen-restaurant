0f73072347d011f79704f685f80c974950bcb0d2
e10ne <etiennevanhalteren@hotmail.com>
2021-09-17 11:59:22 +0200

    Create OrderDish model and related

---
+++ database/factories/OrderDishFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\OrderDish;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class OrderDishFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = OrderDish::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+                //
+            ];
+    }
+}
--- database/migrations/2021_09_17_115703_create_order_dishes_table.php
+++ database/migrations/2021_09_17_115703_create_order_dishes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateOrdersTable extends Migration
+class CreateOrderDishesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,8 @@ class CreateOrdersTable extends Migration
      */
     public function up()
     {
-        Schema::create("orders", function (Blueprint $table) {
+        Schema::create("order_dishes", function (Blueprint $table) {
             $table->id();
-            // $table->foreignId("table_id")->constrained();
-            // $table->foreignId("employee_id")->constrained();
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ class CreateOrdersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists("orders");
+        Schema::dropIfExists("order_dishes");
     }
 }
