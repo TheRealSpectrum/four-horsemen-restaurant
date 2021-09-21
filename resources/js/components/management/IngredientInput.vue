e87543f2a88d8efb1b74731cdfc3947ec97cbcd9
Damy Metzke <damy.metzke@gmail.com>
2021-09-21 14:34:34 +0200

    Implement value loading in `IngredientInput.vue`

--- resources/js/components/management/IngredientInput.vue
+++ resources/js/components/management/IngredientInput.vue
@@ -1,21 +1,36 @@
 <template>
     <div>
         <div class="grid grid-cols-3 h-6">
-            <label for="name" class="text-lg font-bold"> label </label>
+            <label for="name" class="text-lg font-bold"> {{ label }} </label>
             <action-button level="safe">Add ingredient</action-button>
-            <div class="font-bold text-warning-high p-1">errors</div>
+            <div v-if="error" class="font-bold text-warning-high p-1">
+                {{ error }}
+            </div>
         </div>
-        <input
-            name="name"
-            value="old(name, value)"
-            type="type"
-            class="text-lg"
-        />
+        <div class="flex flex-col py-4">
+            <div v-for="(item, index) in value" class="grid grid-cols-3 py-2">
+                <div class="text-lg font-bold">{{ item.type }}</div>
+                <div class="flex flex-row">
+                    <input
+                        type="number"
+                        :name="`ingredient-${index + 1}`"
+                        v-model:value="item.amount"
+                    />
+                    <div>{{ item.unit }}</div>
+                </div>
+                <action-button>Delete</action-button>
+            </div>
+        </div>
     </div>
 </template>
 
 <script lang="js">
 
 export default {
+    props: {
+        label: String,
+        error: String,
+        value: Array,
+    }
 };
 </script>
--- resources/views/components/management/create-field-ingredient.blade.php
+++ resources/views/components/management/create-field-ingredient.blade.php
@@ -1,1 +1,4 @@
-<ingredient-input>{{$slot}}</ingredient-input>
+<ingredient-input
+    label="labal value"
+    name="helloworld"
+    :value="[{type: 'first', amount: 20, unit: 'g'}, {type: 'second', amount: 30, unit: 'ml'}]" />
