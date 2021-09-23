a8b28cc952be1ee25cdcf71ab30cf354b586af10
e10ne <etiennevanhalteren@hotmail.com>
2021-09-23 09:57:12 +0200

    Update orders migration to have user_id instead of employee_id

--- database/migrations/2021_09_17_114204_create_orders_table.php
+++ database/migrations/2021_09_17_114204_create_orders_table.php
@@ -16,7 +16,7 @@ class CreateOrdersTable extends Migration
         Schema::create("orders", function (Blueprint $table) {
             $table->id();
             $table->foreignId("table_id")->constrained();
-            // $table->foreignId("employee_id")->constrained();
+            $table->foreignId("user_id")->constrained();
             $table->timestamps();
         });
     }
