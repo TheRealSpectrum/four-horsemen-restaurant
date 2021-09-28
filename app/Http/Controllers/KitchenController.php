4677dec35f3c332f598ba8f0de4ce7fd301e5791
e10ne <etiennevanhalteren@hotmail.com>
2021-09-28 14:43:47 +0200

    Update KitchenController with empty functions with small description

--- app/Http/Controllers/KitchenController.php
+++ app/Http/Controllers/KitchenController.php
@@ -6,12 +6,34 @@ use Illuminate\Http\Request;
 
 class KitchenController extends Controller
 {
+    // View to see active orders
     public function index()
     {
         return view("kitchen.index");
     }
 
-    public function printReceipt()
+    // Post route to print orders (fake for now)
+    public function printOrder()
+    {
+    }
+
+    // Post route to close orders
+    public function closeOrder()
+    {
+    }
+
+    // Get route to poll new orders
+    public function pollOrder()
+    {
+    }
+
+    // View to edit ingredient in stock
+    public function viewStock()
+    {
+    }
+
+    // Patch route to edit ingredients in stock
+    public function editStock()
     {
     }
 }
