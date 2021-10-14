b6d47c187a3f1453fba38f4de217ef68147d6400
Damy Metzke <damy.metzke@gmail.com>
2021-10-14 20:14:29 +0200

    Style sort arrows

--- resources/views/management/index.blade.php
+++ resources/views/management/index.blade.php
@@ -3,15 +3,15 @@
 @section("management-content")
   <div class="flex flex-col flex-1 m-6">
     <div class="grid grid-cols-8">
-      <div class="col-span-7 h-10 flex flex-row p-4">
+      <div class="col-span-7 h-10 flex flex-row">
         @foreach($editInline ? $builder->inlineColumns : $builder->columns as $column)
           <div class="flex-1 flex flex-row justify-center">
-            <div class="text-center text-lg font-bold">
+            <div class="text-center text-lg font-bold px-2">
               {{ $column->header }}
             </div>
-            <div class="flex flex-col">
-              <a href="{{route("management.$managementName.index")."?page={$models->currentPage()}&sort=$column->column&sort-desc=0"}}" class="sort-arrow-up bg-mono-darker text-sm text-mono-lighter">up</a>
-              <a href="{{route("management.$managementName.index")."?page={$models->currentPage()}&sort=$column->column&sort-desc=1"}}" class="sort-arrow-up bg-mono-darker text-sm text-mono-lighter">down</a>
+            <div class="grid grid-rows-2 h-full">
+              <a href="{{route("management.$managementName.index")."?page={$models->currentPage()}&sort=$column->column&sort-desc=0"}}" class="sort-arrow-up bg-mono-darker block flex-0"></a>
+              <a href="{{route("management.$managementName.index")."?page={$models->currentPage()}&sort=$column->column&sort-desc=1"}}" class="sort-arrow-down bg-mono-darker bloc flex-0"></a>
             </div>
           </div>
 
