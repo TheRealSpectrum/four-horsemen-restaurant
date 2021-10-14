5eb0f12f6e41eb69ab5d0c0d0a6b2c7fddb579eb
Damy Metzke <damy.metzke@gmail.com>
2021-10-14 14:16:47 +0200

    Restyle course list to be more clear

--- resources/js/components/OrderApp.vue
+++ resources/js/components/OrderApp.vue
@@ -59,25 +59,30 @@
                 </div>
             </div>
         </div>
-        <div class="courseList">
-            <label
-                class="courseItem"
-                v-for="(course, index) in order"
-                :key="index"
-                :class="{ selected: selectedCourse == index }"
-            >
-                {{ getLable(course) }}
-                <input
-                    type="radio"
-                    name="course"
-                    :id="`course${index}`"
-                    :value="index"
-                    v-model="selectedCourse"
-                    hidden
-                />
-            </label>
-            <div class="addCourse btn" @click="addCourse()"></div>
-            <div class="addCourse btn" @click="addDrinks()"></div>
+        <div class="courseListWrapper">
+            <div class="courseList">
+                <label
+                    class="courseItem"
+                    v-for="(course, index) in order"
+                    :key="index"
+                    :class="{ selected: selectedCourse == index }"
+                >
+                    {{ getLable(course) }}
+                    <input
+                        type="radio"
+                        name="course"
+                        :id="`course${index}`"
+                        :value="index"
+                        v-model="selectedCourse"
+                        hidden
+                    />
+                </label>
+            </div>
+
+            <div class="courseListAdd">
+                <div class="addCourse btn" @click="addCourse()"></div>
+                <div class="addDrinks btn" @click="addDrinks()"></div>
+            </div>
         </div>
         <div class="btnGroup">
             <action-button level="action" @click-action="placeOrder()"
@@ -444,21 +449,36 @@ export default {
     white-space: nowrap;
     overflow: visible;
 }
-.courseList {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-    gap: 10px;
-    pad: 10px;
-    height: 10%;
+
+.courseListWrapper {
+    display: grid;
     width: 100%;
-    overflow-x: auto;
-    overflow-y: hidden;
-    scroll-snap-type: x proximity;
+    grid-template-columns: 1fr 10rem;
+    height: 20%;
     border-top: 2px solid var(--mono-darker, #000);
     border-bottom: 2px solid var(--mono-darker, #000);
 }
+
+.courseList {
+    display: flex;
+    flex-direction: row;
+    align-items: start;
+    justify-content: start;
+    gap: 10px;
+    pad: 10px;
+    padding: 1rem;
+    width: 100%;
+    overflow-y: hidden;
+    scroll-snap-type: x proximity;
+    flex-wrap: wrap;
+}
+
+.courseListAdd {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+}
+
 .courseItem {
     border: solid 2px var(--mono-darker, #000);
     padding: 10px;
@@ -508,6 +528,40 @@ export default {
     left: calc(50% - 1em);
     top: calc(50% - 0.25em);
     background-color: var(--mono-darker, #000);
+}
+
+.addDrinks {
+    position: relative;
+    order: 9999;
+    height: 3em;
+    width: 3em;
+    flex-shrink: 0;
+    border: solid 0px transparent;
+    border-radius: 90%;
+    /*background: radial-gradient(#fff, #ddd);*/
+    background-color: var(--mono-dark, #fff);
+    border: 2px solid var(--mono-darker, #000);
+    scroll-snap-align: end;
+}
+.addDrinks::before {
+    content: " ";
+    position: absolute;
+    width: 1.5em;
+    height: 2em;
+    flex-shrink: 0;
+    left: calc(50% - 0.75em);
+    top: calc(50% - 1em);
+    background-color: var(--mono-darker, #000);
+}
+.addDrinks::after {
+    content: " ";
+    position: absolute;
+    width: 1.3em;
+    height: 2em;
+    flex-shrink: 0;
+    left: calc(50% - 0.65em);
+    top: calc(50% - 1.1em);
+    background-color: var(--mono-dark, #000);
 }
 
 .btnGroup {
