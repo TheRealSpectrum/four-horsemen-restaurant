e114a210f96f7f6e63eac0c576485ba7bd165408
Damy Metzke <damy.metzke@gmail.com>
2021-10-14 12:03:15 +0200

    Implement pagination for orders
    Page selection has not been implemented.

--- resources/js/components/kitchen/KitchenApp.vue
+++ resources/js/components/kitchen/KitchenApp.vue
@@ -3,7 +3,7 @@
         <div class="flex flex-row"></div>
         <div class="grid grid-cols-3 grid-rows-2 gap-6 p-6">
             <kitchen-item
-                v-for="order in orders"
+                v-for="order in orders[index]"
                 :order-num="order.orderNum"
                 :status="order.status"
                 :course="order.course"
@@ -26,11 +26,12 @@ export default {
         return {
             pollInterval: null,
             orders: [],
+            index: 0,
         };
     },
     methods: {
         poll() {
-            this.orders = [
+            const allOrders = [
                 {
                     orderNum: 1,
                     status: "Ongoing",
@@ -59,7 +60,40 @@ export default {
                     dishes: [],
                     time: "14:00",
                 },
+                {
+                    orderNum: 5,
+                    status: "Ongoing",
+                    course: 2,
+                    dishes: [],
+                    time: "11:00",
+                },
+                {
+                    orderNum: 6,
+                    status: "New",
+                    course: 3,
+                    dishes: [],
+                    time: "12:00",
+                },
+                {
+                    orderNum: 7,
+                    status: "New",
+                    course: 3,
+                    dishes: [],
+                    time: "13:00",
+                },
+                {
+                    orderNum: 8,
+                    status: "New",
+                    course: 3,
+                    dishes: [],
+                    time: "14:00",
+                },
             ]; // todo: implement axios request
+
+            this.orders = [];
+            while (allOrders.length > 0) {
+                this.orders.push(allOrders.splice(0, 6));
+            }
         },
     },
     created() {
