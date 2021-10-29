8ad5e42310b0bf798bd3a5261b791b5a3001e21f
Damy Metzke <damy.metzke@gmail.com>
2021-10-29 09:20:15 +0200

    Implement markups using percentages

--- app/Models/SiteGlobal.php
+++ app/Models/SiteGlobal.php
@@ -9,5 +9,15 @@ class SiteGlobal extends Model
 {
     use HasFactory;
 
+    public function markupDishesUnit(): float
+    {
+        return $this->markup_dishes / 100;
+    }
+
+    public function markupDrinksUnit(): float
+    {
+        return $this->markup_drinks / 100;
+    }
+
     protected $fillable = ["markup_dishes", "markup_drinks"];
 }
