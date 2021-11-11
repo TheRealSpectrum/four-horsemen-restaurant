83681a3180e8e061689d02ab32441730f6baf529
Damy Metzke <damy.metzke@gmail.com>
2021-11-11 10:32:11 +0100

    Improve seeding for categories

--- database/seeders/DrinkSeeder.php
+++ database/seeders/DrinkSeeder.php
@@ -13,16 +13,25 @@ class DrinkSeeder extends Seeder
             "name" => "White Wine",
             "price" => 5000,
             "alcohol_content" => 10,
+            "category_id" => 2,
         ]);
         Drink::factory()->create([
             "name" => "Red Wine",
             "price" => 5500,
             "alcohol_content" => 11,
+            "category_id" => 2,
         ]);
         Drink::factory()->create([
             "name" => "Beer",
             "price" => 600,
+            "alcohol_content" => 6,
+            "category_id" => 1,
+        ]);
+        Drink::factory()->create([
+            "name" => "White Beer",
+            "price" => 600,
             "alcohol_content" => 5,
+            "category_id" => 1,
         ]);
     }
 }
